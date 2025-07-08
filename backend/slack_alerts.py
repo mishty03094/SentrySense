bd1d74d0558af1035c51abd439a06a08f0da4c35
Mishthi27 <mishthisachdeva27@gmail.com>
2025-07-08 19:24:11 +0530

    Update slack_alerts.py

--- backend/slack_alerts.py
+++ backend/slack_alerts.py
@@ -1,11 +1,19 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"  # replace with your real one
+load_dotenv()  # Load from .env file
+
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
 
 def send_slack_alert(message: str, blocks: list = None):
+    if not SLACK_WEBHOOK_URL:
+        print("Slack Webhook URL not found in environment.")
+        return
+
     payload = {"text": message}
     if blocks:
-        payload["blocks"] = blocks  # for rich formatting
+        payload["blocks"] = blocks
 
     response = requests.post(SLACK_WEBHOOK_URL, json=payload)
     if response.status_code != 200:
